2f96f4bc20161e6af414fd384d94dfea022f7861
Hiruni Thenuwara <hirunikethaki17@gmail.com>
2026-08-31 21:21:16 +0530

    add doctor management page

---
+++ src/main/java/com/sunrise/controller/DoctorServlet.java
@@ -0,0 +1,30 @@
+package com.sunrise.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Dentist management screen.
+ *
+ * <p>Day 2 version: forwards to the view so the interface can be reviewed
+ * and tested. The DAO calls and the add / edit / delete actions are added
+ * on Day 3.</p>
+ */
+@WebServlet(name = "DoctorServlet", urlPatterns = {"/admin/doctors"})
+public class DoctorServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.setAttribute("activePage", "doctors");
+        request.setAttribute("pageTitle", "Dentists");
+        request.getRequestDispatcher("/WEB-INF/views/doctors.jsp").forward(request, response);
+    }
+}
